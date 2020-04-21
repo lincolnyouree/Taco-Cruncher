cd81800a3767201fe3dd4ea71adf450068f65bc5
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-21 14:17:13 -0500

    Updates

---
+++ precache-manifest.ec129eb313d67ed8957f72d920b47c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e45ec3d602a38af92e2592617a7857",
+    "url": "/Taco-Cruncher/index.html"
+  },
+  {
+    "revision": "2b2626ecbe215d196147",
+    "url": "/Taco-Cruncher/static/css/main.949bd4d4.chunk.css"
+  },
+  {
+    "revision": "913b910d81a0cf1b38c1",
+    "url": "/Taco-Cruncher/static/js/2.1472dc98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Taco-Cruncher/static/js/2.1472dc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b2626ecbe215d196147",
+    "url": "/Taco-Cruncher/static/js/main.14ee62c1.chunk.js"
+  },
+  {
+    "revision": "23f8932e7bd803394e0d",
+    "url": "/Taco-Cruncher/static/js/runtime-main.ce93a432.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Taco-Cruncher/static/media/logo.5d5d9eef.svg"
+  }
+]);
